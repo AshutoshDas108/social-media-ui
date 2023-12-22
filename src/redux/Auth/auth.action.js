05e2aeb2a6a8885e353735446cd089d1a1acb0b1
AshutoshDas <ashutosh.rkl.2001@gmail.com>
2023-12-23 01:01:02 +0530

    register action function added

--- src/redux/Auth/auth.action.js
+++ src/redux/Auth/auth.action.js
@@ -1,8 +1,11 @@
 import { API_BASE_URL } from "../../config/api"
-import { LOGIN_FAILURE, LOGIN_SUCCESS } from "./auth.actionType"
+import { LOGIN_FAILURE, LOGIN_SUCCESS, REGISTER_FAILURE, REGISTER_SUCCESS } from "./auth.actionType"
+
 
 const loginUserAction = (loginData) => async (dispatch) => {
-    try {
+    
+    try 
+    {
         const {data} = await axios.post(`${API_BASE_URL}/auth/login`, loginData)
 
         if(data.jwt) {
@@ -11,10 +14,35 @@ const loginUserAction = (loginData) => async (dispatch) => {
 
         dispatch({type:LOGIN_SUCCESS, payload: data.jwt})
 
-    } catch (error) {
+    } 
+    
+    catch (error) {
 
         console.log("----->" , error.message)
         dispatch({type:LOGIN_FAILURE, payload:error})
+        
+    }
+}
+
+
+const registerUserAction = (registerData) => async (dispatch) => {
+    
+    try 
+    {
+        const {data} = await axios.post(`${API_BASE_URL}/auth/register`, registerData)
+
+        if(data.jwt) {
+            localStorage.setItem('jwt', data.jwt)
+        }
+
+        dispatch({type:REGISTER_SUCCESS, payload: data.jwt})
+
+    } 
+    
+    catch (error) {
+
+        console.log("----->" , error.message)
+        dispatch({type:REGISTER_FAILURE, payload:error})
         
     }
 }
